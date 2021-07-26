4a933a85a0aac76cda72f9a83586d9430ed922e9
Wadoo-Gazoono <blopfly0@gmail.com>
2021-07-26 10:21:08 -0600

    Clean up

--- src/main/java/com/Wadoo/hyperion/Client/ClientListener.java
+++ src/main/java/com/Wadoo/hyperion/Client/ClientListener.java
@@ -1,8 +1,11 @@
 package com.Wadoo.hyperion.Client;
 
+import com.Wadoo.hyperion.Client.Renderer.BasaltCapslingRenderer;
 import com.Wadoo.hyperion.Hyperion;
+import com.Wadoo.hyperion.Server.Register.EntityRegister;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -10,5 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class ClientListener {
     @SubscribeEvent
     public static void registerRenderers(final FMLClientSetupEvent event) {
+        RenderingRegistry.registerEntityRenderingHandler(EntityRegister.BASALT_CAPSLING.get(),
+                manager -> new BasaltCapslingRenderer(manager));
     }
 }
