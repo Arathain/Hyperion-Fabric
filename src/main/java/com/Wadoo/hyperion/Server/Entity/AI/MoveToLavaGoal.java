6ed49faaa23a3a023fe00cb4d37ba010bfd0a675
Wadoo-Gazoono <blopfly0@gmail.com>
2021-07-26 18:16:17 -0600

    Adding basic Capsling AI

---
+++ src/main/java/com/Wadoo/hyperion/Server/Entity/AI/MoveToLavaGoal.java
@@ -0,0 +1,38 @@
+package com.Wadoo.hyperion.Server.Entity.AI;
+
+import com.Wadoo.hyperion.Server.Entity.BasaltCapslingEntity;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.ai.goal.MoveToBlockGoal;
+import net.minecraft.entity.passive.StriderEntity;
+import net.minecraft.pathfinding.PathType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
+
+public class MoveToLavaGoal extends MoveToBlockGoal {
+    private final BasaltCapslingEntity entity;
+
+    public MoveToLavaGoal(BasaltCapslingEntity entity, double speedModifier) {
+        super(entity, speedModifier, 8, 2);
+        this.entity = entity;
+    }
+
+    public BlockPos getMoveToTarget() {
+        return this.blockPos;
+    }
+
+    public boolean canContinueToUse() {
+        return !this.entity.isInLava() && this.isValidTarget(this.entity.level, this.blockPos);
+    }
+
+    public boolean canUse() {
+        return !this.entity.isInLava() && super.canUse();
+    }
+
+    public boolean shouldRecalculatePath() {
+        return this.tryTicks % 20 == 0;
+    }
+
+    protected boolean isValidTarget(IWorldReader worldReader, BlockPos blockPos) {
+        return worldReader.getBlockState(blockPos).is(Blocks.LAVA) && worldReader.getBlockState(blockPos.above()).isPathfindable(worldReader, blockPos, PathType.LAND);
+    }
+}
--- src/main/java/com/Wadoo/hyperion/Server/Entity/BasaltCapslingEntity.java
+++ src/main/java/com/Wadoo/hyperion/Server/Entity/BasaltCapslingEntity.java
@@ -1,7 +1,26 @@
 package com.Wadoo.hyperion.Server.Entity;
 
+import com.Wadoo.hyperion.Server.Entity.AI.MoveToLavaGoal;
+import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.RandomWalkingGoal;
+import net.minecraft.entity.monster.MagmaCubeEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.pathfinding.PathNodeType;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -13,16 +32,42 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 public class BasaltCapslingEntity extends CreatureEntity implements IAnimatable {
     private final AnimationFactory factory = new AnimationFactory(this);
-
+    private static final DataParameter<Boolean> OPEN = EntityDataManager.defineId(BasaltCapslingEntity.class, DataSerializers.BOOLEAN);
 
     public BasaltCapslingEntity(EntityType<? extends CreatureEntity> type, World world) {
         super(type, world);
+
+        this.setPathfindingMalus(PathNodeType.WATER, -1.0F);
+        this.setPathfindingMalus(PathNodeType.LAVA, 0.0F);
+        this.setPathfindingMalus(PathNodeType.DANGER_FIRE, 0.0F);
+        this.setPathfindingMalus(PathNodeType.DAMAGE_FIRE, 0.0F);
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        event.getController().setAnimation(new AnimationBuilder()
-                .addAnimation("animation.basalt_capsling.idle", true));
-        return PlayState.CONTINUE;
+        if(event.isMoving() || this.isInLava()){
+            event.getController().setAnimation(new AnimationBuilder()
+                    .addAnimation("animation.basalt_capsling.walk", true));
+            return PlayState.CONTINUE;
+        }
+        else {
+            event.getController().setAnimation(new AnimationBuilder()
+                    .addAnimation("animation.basalt_capsling.idle", true));
+            return PlayState.CONTINUE;
+        }
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(OPEN, false);
+    }
+
+    public boolean getOpen() {
+        return this.entityData.get(OPEN);
+    }
+
+    public void setOpen(boolean open){
+        this.entityData.set(OPEN, open);
     }
 
     @Override
@@ -31,7 +76,34 @@ public class BasaltCapslingEntity extends CreatureEntity implements IAnimatable 
     }
 
     @Override
+    protected void registerGoals() {
+        super.registerGoals();
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.addGoal(7, new RandomWalkingGoal(this, 0.8D));
+        this.goalSelector.addGoal(6, new LookAtGoal(this, BasaltCapslingEntity.class, 8.0F));
+        this.goalSelector.addGoal(2, new MoveToLavaGoal(this, 1.0D));
+    }
+
+    @Override
     public AnimationFactory getFactory() {
         return this.factory;
     }
+
+    private void floatCapsling() {
+        if (this.isInLava()) {
+            ISelectionContext iselectioncontext = ISelectionContext.of(this);
+            if (iselectioncontext.isAbove(FlowingFluidBlock.STABLE_SHAPE, this.blockPosition(), true) && !this.level.getFluidState(this.blockPosition().above()).is(FluidTags.LAVA)) {
+                this.onGround = true;
+            } else {
+                this.setDeltaMovement(this.getDeltaMovement().scale(0.5D).add(0.0D, 0.05D, 0.0D));
+            }
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        this.floatCapsling();
+    }
+
 }
