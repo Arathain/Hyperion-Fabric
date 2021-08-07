9406a4e4bc5910a161e6bde66a739cc0a8d7d9fb
Wadoo-Gazoono <blopfly0@gmail.com>
2021-08-07 16:23:24 -0600

    Finishing Basalt Arrows

--- src/main/java/com/Wadoo/hyperion/Server/Item/BasaltArrowItem.java
+++ src/main/java/com/Wadoo/hyperion/Server/Item/BasaltArrowItem.java
@@ -3,6 +3,7 @@ package com.Wadoo.hyperion.Server.Item;
 import com.Wadoo.hyperion.Hyperion;
 import com.Wadoo.hyperion.Server.Entity.BasaltArrowEntity;
 import com.Wadoo.hyperion.Server.Register.EntityRegister;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
@@ -10,7 +11,15 @@ import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ArrowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class BasaltArrowItem extends ArrowItem {
 
@@ -27,4 +36,10 @@ public class BasaltArrowItem extends ArrowItem {
         return enchant <= 0 ? false : this.getClass() == BasaltArrowItem.class;
     }
 
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        super.appendHoverText(stack, worldIn, tooltip, flagIn);
+        tooltip.add(new StringTextComponent("Travels shorter distances"));
+        tooltip.add(new StringTextComponent("Deals more damage the more armour your target has"));
+    }
 }
