8c569752b3a30d451e32bc475b59905ce6fc3aa9
Wadoo-Gazoono <blopfly0@gmail.com>
2021-12-22 21:01:44 -0700

    Port to 1.18, fixing file organization

--- src/main/java/com/Wadoo/hyperion/servers/registry/TagRegister.java
+++
@@ -1,9 +0,0 @@
-package com.Wadoo.hyperion.servers.registry;
-
-import com.Wadoo.hyperion.Hyperion;
-import net.minecraft.resources.ResourceLocation;
-
-public class TagRegister {
-    public static final ResourceLocation CAPSLING_WANTED = new ResourceLocation(Hyperion.MOD_ID, "capsling_accepted_blocks");
-
-}
