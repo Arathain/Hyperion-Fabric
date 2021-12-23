9798bae214659927328cc32acc3dbde84f6c7263
Wadoo-Gazoono <blopfly0@gmail.com>
2021-12-22 20:47:42 -0700

    Port to 1.18, fixing file organization

---
+++ src/main/java/com/Wadoo/hyperion/Client/Renderer/BasaltBanneretRenderer.java
@@ -0,0 +1,16 @@
+package com.Wadoo.hyperion.client.renderer;
+
+import com.Wadoo.hyperion.client.model.BasaltBanneretModel;
+import com.Wadoo.hyperion.server.entity.BasaltBanneretEntity;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class BasaltBanneretRenderer extends GeoEntityRenderer<BasaltBanneretEntity> {
+    public BasaltBanneretEntity entity;
+
+    public BasaltBanneretRenderer(EntityRendererProvider.Context renderManager) {
+        super(renderManager, new BasaltBanneretModel());
+        this.shadowRadius = 0.6F;
+    }
+
+}
